5e861de430cda9a37cab4894859b60557f6e42b5
David Corne <davidcorne@gmail.com>
2015-09-11 12:56:32 +0000

    REset the JS to basic JS.

--- src/js/pebble-js-app.js
+++ src/js/pebble-js-app.js
@@ -1,28 +1,3 @@
-Pebble.addEventListener('ready', function() {
+Pebble.addEventListener('ready', function(e) {
   console.log('PebbleKit JS ready!');
-});
-
-Pebble.addEventListener('showConfiguration', function() {
-  var url = 'https://kirby.ngrok.io';
-
-  console.log('Showing configuration page: ' + url);
-
-  Pebble.openURL(url);
-});
-
-Pebble.addEventListener('webviewclosed', function(e) {
-  var configData = JSON.parse(decodeURIComponent(e.response));
-
-  console.log('Configuration page returned: ' + JSON.stringify(configData));
-
-  if (configData.backgroundColor) {
-    Pebble.sendAppMessage({
-      backgroundColor: parseInt(configData.backgroundColor, 16),
-      twentyFourHourFormat: configData.twentyFourHourFormat
-    }, function() {
-      console.log('Send successful!');
-    }, function() {
-      console.log('Send failed!');
-    });
-  }
 });
